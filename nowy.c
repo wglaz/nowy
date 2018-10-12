b783efe81b8ecc922f028c8901347a05b043119a
wglaz <wojciu6666@gmail.com>
2018-10-12 10:57:55 +0200

    wyswietlanie kilku rzeczy

--- nowy.c
+++ nowy.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    char znak;\
-    scanf("%c", &znak);
-    putchar(znak);
+    char znak;
+    int wartosc_calkowita;
+    double wartosc_wymierna;
+
+    scanf("%c%i%lf",
+          &znak,
+          &wartosc_calkowita,
+          &wartosc_wymierna);
+
+    printf(
+            "znak = %c,"
+            "\n liczba całkowita = %i,"
+            "\n liczba wymierna = %lf",
+            znak,
+            wartosc_calkowita,
+            wartosc_wymierna
+
+    );
+
     return 0;
 
 }
