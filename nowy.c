a2aa1878763369740ca2bc8a6c71cbd7422fcc46
wglaz <wojciu6666@gmail.com>
2018-10-12 14:03:51 +0200

    opisy do wprowadzania i wyswietlanie kilku rzeczy ---działa

--- nowy.c
+++ nowy.c
@@ -7,16 +7,14 @@ int main() {
     char ciag_znakow[256]; //tablica znaków ciąg znaków
 
     printf("podaj znak "); scanf("%c", &znak);
-    printf("podaj ciąg znakowy "); scanf("%s", ciag_znakow);
+    printf("podaj ciąg znakowy "); scanf("%s", &ciag_znakow);
     printf("podaj liczbe całkowitą "); scanf("%i", &wartosc_calkowita);
     printf("podaj liczbe wymierną "); scanf("%lf", &wartosc_wymierna);
 
     printf("\n");
     printf(
-            "znak = %c,\n",
-            "ciąg znakowy = %s\n",
-            "liczba całkowita = %i",
-            "\nliczba wymierna = %lf",
+            "znak = %c,\nciąg znakowy = %s,\n"
+            "liczba całkowita = %i,\nliczba wymierna = %lf",
 
             znak,
             ciag_znakow,
