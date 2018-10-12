4176f3a46b1212662a6a413bb073dba93fb38268
wglaz <wojciu6666@gmail.com>
2018-10-12 11:20:26 +0200

    coś przestało działać

--- nowy.c
+++ nowy.c
@@ -4,15 +4,22 @@ int main() {
     char znak;
     int wartosc_calkowita;
     double wartosc_wymierna;
+    char ciag_znakow[256]; //tablica znaków ciąg znaków
 
     printf("podaj znak "); scanf("%c", &znak);
+    printf("podaj ciąg znakowy "); scanf("%s", ciag_znakow);
     printf("podaj liczbe całkowitą "); scanf("%i", &wartosc_calkowita);
     printf("podaj liczbe wymierną "); scanf("%lf", &wartosc_wymierna);
+
+    printf("\n");
     printf(
-            "znak = %c,"
-            "\n liczba całkowita = %i,"
-            "\n liczba wymierna = %lf",
+            "znak = %c,\n",
+            "ciąg znakowy = %s\n",
+            "liczba całkowita = %i",
+            "\nliczba wymierna = %lf",
+
             znak,
+            ciag_znakow,
             wartosc_calkowita,
             wartosc_wymierna
 
